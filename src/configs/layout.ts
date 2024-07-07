93c2f2715c14a3eb79afd28b8ff4293148a72faf
anhtran8301 <101809193+anhtran8301@users.noreply.github.com>
2024-07-07 17:51:29 +0700

    improve ui vertical layout

---
+++ src/configs/layout.ts
@@ -0,0 +1,55 @@
+export const VerticalItems = [
+    {
+        title: "Parent 1",
+        icon: "ic:outline-more",
+        path: "/",
+        childrens: [
+            {
+                title: "Child 1 1",
+                icon: "ic:outline-more",
+                path: "/",
+                childrens: [
+                    {
+                        title: "Child 1 1 1",
+                        icon: "ic:outline-more",
+                        path: "/",
+                    },
+                    {
+                        title: "Child 1 1 2",
+                        icon: "ic:outline-more",
+                        path: "/",
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        title: "Parent 2",
+        icon: "ic:outline-more",
+        path: "/",
+        childrens: [
+            {
+                title: "Child 2 1",
+                icon: "ic:outline-more",
+                path: "/",
+                childrens: [
+                    {
+                        title: "Child 2 1 1",
+                        icon: "ic:outline-more",
+                        path: "/",
+                    },
+                    {
+                        title: "Child 2 1 2",
+                        icon: "ic:outline-more",
+                        path: "/",
+                    },
+                    {
+                        title: "Child 2 1 3",
+                        icon: "ic:outline-more",
+                        path: "/",
+                    },
+                ],
+            },
+        ],
+    },
+]
